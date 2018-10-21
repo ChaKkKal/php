a6cfd00e8230f56fa6b9c4a359f2c46920afe29b
ChaKkal <gabriel.cunha04@gmail.com>
2018-10-21 18:47:20 -0300

    Sou burrinho mas já fiz certo o tutorial

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
 
-<<<<<<< HEAD
     echo "Olá Mundo456!";
 
-    echo "Olá udemy";
+    echo "Olá udemy e hcode";
 
-=======
-    echo "Olá Mundo123!";
-
-    echo "Olá hcode";
->>>>>>> 2c12140a42dcc8d76a2193ea8a51ec3036eb4a38
 
 ?>
